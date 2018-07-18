c3158b4b402df245af702bead27926cd1956552c
Varlen Pavani Neto <varlenneto@poli.ufrj.br>
2018-07-17 21:47:00 -0300

    Implementa heatmap

--- codigo/house_prices.py
+++ codigo/house_prices.py
@@ -13,18 +13,72 @@ warnings.filterwarnings('ignore')
 df_train = pd.read_csv('./dataset/train.csv')
 
 # Observação estatistica dos dados
-for col in df_train.columns:
-    print(df_train[col].describe())
+#for col in df_train.columns:
+#    print(df_train[col].describe())
+
+
+variaveis_a_desconsiderar = [
+        
+        # Numericas
+       'Id','MSSubClass','SalePrice', 
+       'OverallQual', 'OverallCond','YearRemodAdd',
+       'BsmtFinSF2', '2ndFlrSF', 
+       'LowQualFinSF', 'BsmtFullBath',
+       'BsmtHalfBath','FullBath','HalfBath',
+       'BedroomAbvGr','KitchenAbvGr',
+       'Fireplaces','GarageCars', 'EnclosedPorch',
+       '3SsnPorch','ScreenPorch','PoolArea',
+       'MiscVal','MoSold','YrSold','TotRmsAbvGrd'
+       
+       # Categoricas
+       'MSZoning','Street','LotShape','Utilities','LandSlope',
+       'BldgType','Condition1','RoofStyle','Exterior1st',
+       'Exterior2nd','MasVnrType','Foundation','BsmtCond','BsmtExposure',
+       'BsmtFinType1','BsmtFinType2','Heating','HeatingQC','CentralAir',
+       'Electrical','Functional','GarageType','GarageFinish','GarageQual',
+       'GarageCond','PavedDrive','Fence','MiscFeature',
+       'SaleType','SaleCondition','RoofMatl','Condition2','HouseStyle'
+       
+]
 
 # Scatterplots das variaveis numericas
 variaveis_numericas = [ col 
                        for col in df_train.columns 
                        if df_train[col].describe().dtype == 'float64' 
-                       and not col in ['Id','SalePrice'] ]
+                       and not col in variaveis_numericas_a_desconsiderar ]
+
 for var in variaveis_numericas :
     data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-    data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+    data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+
+# Boxplot das variaveis categoricas    
+variaveis_categoricas = [ col for col in df_train.columns 
+                         if not col in variaveis_a_desconsiderar
+                         and not col in variaveis_numericas ]
+variaveis_categoricas.append('OverallQual')
+variaveis_categoricas.append('OverallCond')
+
+
+for var in variaveis_categoricas:
+    
+    sns.boxplot(y = "SalePrice", x = var, data = df_train)
+    sns.stripplot(y = "SalePrice", x = var, data = df_train)
+    plt.show()
+    
     
 # Scatterplots razoavelmente correlacionados em relação a SalePrice
 lin_cols = ['LotFrontage','YearBuilt','TotalBsmtSF',
             '1stFlrSF', 'GrLivArea','GarageArea','WoodDeckSF']
+
+
+# Matriz de correlacao 
+k = 11 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+f, aax = plt.subplots(figsize=(12,9))
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+
+
--- relatorio.md
+++ relatorio.md
@@ -1,37 +1,29 @@
 ## Entendimento do conjunto de dados
 
-O conjunto de dados fornecido contempla 81 colunas com 1460 registros para teste e 1460 registros para treino.
-Inicialmente utilizou-se um software de planilha para observação dos dados em forma tabular. As variáveis também foram utilizadas para criação de gráficos de espalhamento, possibilitando estimar a influência individual de cada uma destas.
+O conjunto de dados fornecido contempla 81 colunas com 1460 registros para teste e 1460 registros para treino. Utilizou-se a caracterização presente no arquivo data_description.txt para ter o significado de cada variável.
+
+Para obter uma inferência quantitativa menos heuristica, inicialmente as variáveis numéricas foram utilizadas para criação de gráficos de espalhamento, possibilitando estimar a influência individual de cada uma destas.
+
+OverallQual -> 0 a 10
+
+MiscVal --> Pode ser uma caracteristica qualquer. Devo descontar do valor final?
+
+
+---
+
+----- LotShape vs LandContour
+
+Ambas categorias parecem consideravelmente correlacionadas, porém LandContour parece mais regular.
+
+Foi gerado um mapa de calor incluindo as 10 variáveis mais correlacionadas com o preço da venda. 
+
+<img src="./imagens/hm.png">
+
+O mapa de calor mostra que as variáveis GrLivArea e TotRmsAbvGrd, TotalBsmtSF e 1stFlrSF são altamente correlacionadas. Assim, TotalRmsAbvGrd e TotalBsmtSF foram desconsideradas. 
 
 Através desta observação preliminar do conjunto de dados, foi feita a caracterização das diferentes variáveis disponíveis. 
 
-<table>
-    <thead>
-        <th>Variável</th>
-        <th>Tipo</th>
-        <th>Categoria</th>
-        <th>Influência Esperada</th>
-        <th>Incluir?</th>
-        <th>Obs</th>
-    </thead>
-    <tr>
-        <td>MSSubClass</td>
-        <td>Discreta</td>
-        <td>-</td>
-        <td>Baixa</td>
-        <td>Não</td>
-        <td>- </td>
-    </tr>
-    <tr>
-        <td>MSZoning</td>
-        <td>Discreta</td>
-        <td>-</td>
-        <td>Baixa</td>
-        <td>Não</td>
-        <td>1151 de 1460 entradas apresentam valor RL, com 5 valores distintos.</td>
-    </tr>
 
 
-</table>
 
 Com intuito de simplificar o conjunto de dados, algumas colunas foram desconsideradas. São essas a coluna Street que dentro dos dados de treino sempre apresentou o valor "Pave", Utilities que com exceção de um registro, apresentou valor "AllPub", 
